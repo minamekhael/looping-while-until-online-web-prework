62c765677e28f221dfad7199eca64f762354153f
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-02 23:01:32 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
   levitation_force = 0
  until levitation_force == 10
- levitation_force += 2
+ levitation_force += 1
       puts "Wingardium Leviosa"
       "Wingardium Leviosa"
 end
